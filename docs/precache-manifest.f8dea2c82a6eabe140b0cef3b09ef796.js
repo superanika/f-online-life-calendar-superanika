cee82e46cba8bf6225e07a3751c0c27b1d31381d
superanika <anamaria.romerogarcia@gmail.com>
2019-07-01 15:26:37 +0200

    docs

---
+++ docs/precache-manifest.f8dea2c82a6eabe140b0cef3b09ef796.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3561d366020e8331e4ae01518fc788d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6c7fce5416bb45656f6",
+    "url": "./static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "d68400e036e251692ffe",
+    "url": "./static/css/main.4ca049e0.chunk.css"
+  },
+  {
+    "revision": "e6c7fce5416bb45656f6",
+    "url": "./static/js/2.1605abae.chunk.js"
+  },
+  {
+    "revision": "d68400e036e251692ffe",
+    "url": "./static/js/main.a4578476.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "4578dd21d0aa5b55e5d4d126964105ea",
+    "url": "./static/media/184974_smiley-wallpaper-hd.4578dd21.jpg"
+  }
+]);
